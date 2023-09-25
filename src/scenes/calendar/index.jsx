140c8594416c058f5dded403de4acc26502b48cb
Guillermo-OC <guillermo.valdez.oc@gmail.com>
2023-09-24 21:13:22 -0600

    agregado del apartado de calendar

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -74,6 +74,36 @@ const Calendar = () => {
               ))}
             </List>
           </Box>
+          {/* CALENDAR */}
+          <Box flex="1 1 100%" ml="15px">
+            <FullCalendar
+                height="75vh"
+                plugins={[
+                    dayGridPlugin,
+                    timeGridPlugin,
+                    interactionPlugin,
+                    listPlugin
+                ]}
+                headerToolbar={{
+                    left: "prev,next today",
+                    center: "title",
+                    right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth"
+                }}
+                initialView="dayGridMonth"
+                editable={true}
+                selectable={true}
+                selectMirror={true}
+                dayMaxEvents={true}
+                select={handleDateClick}
+                eventClick={handleEventClick}
+                eventsSet={(events) => setCurrentEvents(events)}
+                initialEvents={[
+                    {id: "1234", title: "All-day-event", date: "2022-09-14"},
+                    {id: "4321", title: "Timed event", date: "2022-09-25"},
+                    {id: "1111", title: "Junta con Alain", date: "2022-09-30"},
+                ]}
+                />
+          </Box>
         </Box>
       </Box>
     );
